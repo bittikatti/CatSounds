a18b4ae46d5586d3b93b3cf077f2284bb479cd87
bittikatti <28701834+bittikatti@users.noreply.github.com>
2025-12-15 15:25:23 +0200

    Add D1 and sql schema for it

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS CatSounds;
+
+CREATE TABLE IF NOT EXISTS CatSounds (
+    CatSoundID     INTEGER PRIMARY KEY AUTOINCREMENT,
+    Transcript     TEXT,
+    SoundGroup     TEXT,
+    SoundFileLink  TEXT,
+    SoundLicence   TEXT
+);
