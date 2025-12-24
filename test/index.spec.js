dcd3aa19ae0c30543e7ce4298153b71f20b5182f
bittikatti <28701834+bittikatti@users.noreply.github.com>
2025-12-24 14:25:32 +0200

    Create test database and use it in tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,18 +3,18 @@ import { describe, it, expect } from 'vitest';
 import worker from '../src';
 
 describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
+	it('responds with welcome (unit style)', async () => {
 		const request = new Request('http://example.com');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toContain("Welcome to the D1 API Playground");
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('http://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('responds with welcome (integration style)', async () => {
+		const response = await SELF.fetch('http://example.com/api/sounds');
+		expect(await response.text()).toContain("CatSoundID");
 	});
 });
---
+++ test/unit.spec.js
@@ -0,0 +1,24 @@
+import {
+    env,
+    createExecutionContext,
+    waitOnExecutionContext,
+} from "cloudflare:test";
+import { describe, it, expect } from "vitest";
+// Import your worker so you can unit test it
+import worker from "../src";
+
+// For now, you'll need to do something like this to get a correctly-typed
+// `Request` to pass to `worker.fetch()`.
+const IncomingRequest = Request;
+
+describe("local worker alive", () => {
+    it("responds", async () => {
+        const request = new IncomingRequest("http://example.com/api/sounds");
+        // Create an empty context to pass to `worker.fetch()`
+        const ctx = createExecutionContext();
+        const response = await worker.fetch(request, env, ctx);
+        // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+        await waitOnExecutionContext(ctx);
+        expect(response.status).toBe(200);
+    });
+});
